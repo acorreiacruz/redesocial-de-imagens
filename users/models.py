a08ddb7ba82e4f30f14b355f6978933c48d7373b
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-26 01:22:35 -0300

    Criando um model User customizado.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ("-id",)
+
+    first_name = models.CharField(max_length=150, blank=True, null=False)
+    last_name = models.CharField(max_length=150, blank=True, null=False)
+    username = models.CharField(max_length=150, blank=False, null=False, unique=True)
+    email = models.CharField(max_length=150, blank=False, null=False, unique=True)
+    phone_number = PhoneNumberField(blank=False, null=False, max_length=20, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["username","email"]
+
+    def __str__(self):
+        return self.username
