c50e6c2e56d70e505d3a36707fb49f58f64e0d66
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-26 02:23:18 -0300

    Criando o model Post.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin, BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
@@ -75,3 +76,24 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return f"Profile of: {self.user.username}"
 
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = "Post"
+        verbone_name_plural = "Posts"
+        ordering = ("-id",)
+        db_table = "posts"
+
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        photo = models.ImageField(upload_to="posts/photos/%Y/%m/%d/")
+        text = models.CharField(max_length=2200, null=False, blank=False)
+        likes = models.PositiveIntegerField(default=0)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+            return f"Post of user:{self.user.username}"
+
+        @property
+        def username(self):
+            return self.user.username
