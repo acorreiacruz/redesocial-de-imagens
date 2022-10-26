dcc5097c7d5ad35023e5b98912348198af49771d
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-26 02:32:16 -0300

    Criando o model PostLike.

--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin, BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
@@ -97,3 +96,25 @@ class Post(models.Model):
         @property
         def username(self):
             return self.user.username
+
+
+class PostLike(models.Model):
+    class Meta:
+        verbose_name = "PostLike"
+        verbone_name_plural = "PostLikes"
+        ordering = ("-id",)
+        db_table = "posts_likes"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post {self.id} liked by {self.user.username}"
+
+    @property
+    def username(self):
+        return self.user.username
+
+    @property
+    def post_id(self):
+        return self.post.id
