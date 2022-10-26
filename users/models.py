1ff8d0b14962c1d1bc416b5ce9d99d96450c5f29
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-26 02:43:14 -0300

    Criando o model Remark.

--- users/models.py
+++ users/models.py
@@ -118,3 +118,30 @@ class PostLike(models.Model):
     @property
     def post_id(self):
         return self.post.id
+
+
+
+class Remark(models.Model):
+    class Meta:
+        verbose_name = "Remark"
+        verbone_name_plural = "Remark"
+        ordering = ("-id",)
+        db_table = "remarks"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=2200, null=False, blank=False)
+    likes = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Remark {self.id} by {self.user.username} on post {self.post.id}"
+
+    @property
+    def username(self):
+        return self.user.username
+
+    @property
+    def post_id(self):
+        return self.user.username
