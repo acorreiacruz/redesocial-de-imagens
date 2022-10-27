413005acced6f2ed3bc772f9724f7c791be2dc25
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-27 19:28:40 -0300

    Subscrevendo o método create do UserSerializer.

--- api/serializers.py
+++ api/serializers.py
@@ -9,3 +9,10 @@ class UserSerializer(serializers.ModelSerializer):
             "id", "first_name", "last_name",
             "username", "email", "phone_number", "password"
         ]
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(password=password, **validated_data)
+        user.set_password(password)
+        user.save()
+        return user
