426a083aee98b10dfd16421d529f134b2685232e
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-27 18:03:22 -0300

    Alterano a url para o app users.

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include("socialnetwork.urls")),
-    path('users/', include("users.urls")),
+    path('api/users/', include("users.urls")),
 ]
 
 if settings.DEBUG:
