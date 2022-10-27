8701ca1b710b4bbfe604c8597fc8917e92929a90
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-26 22:32:23 -0300

    Configurando o admin do model User.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile, User, Tag, Remark, RemarkLike, Post, PostLike, Following
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdminConfig(UserAdmin):
+    list_display = "id", "username", "email", "is_active", "is_staff"
+    list_display_links = "username", "email"
+    list_per_page = 25
+    fieldsets = (
+        (
+            "Authentication Fields",
+            {
+                "fields": ("username","email", "phone_number"),
+            }
+        ),
+        (
+            None,
+            {
+                "fields": ("password",),
+            }
+        ),
+        (
+            "Personal Info",
+            {
+                "fields": ("first_name","last_name"),
+            }
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions"
+                ),
+            }
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login","date_joined"),
+            }
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdminConfig(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Remark)
+class RemarkAdminConfig(admin.ModelAdmin):
+    ...
+
+@admin.register(Post)
+class PostAdminConfig(admin.ModelAdmin):
+    ...
