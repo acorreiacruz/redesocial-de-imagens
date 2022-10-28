0f2eda67ff5e59b7bf8f8665925bb7683ca5ed31
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-27 21:27:06 -0300

    Configurando o rest_framework e o simple JWT.

--- core/settings/rest_framework.py
+++ core/settings/rest_framework.py
@@ -1,5 +1,25 @@
+from datetime import timedelta
+import os
+
+
 REST_FRAMEWORK = {
-    "DEFAULT_RENDERER_CLASSES":[
-        "rest_framework.renderers.JSONRenderer"
+    'DEFAULT_RENDERERS_CLASSES':[
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'SIGNING_KEY': os.environ.get('SECRET_KEY'),
+    'AUTH_HEADER_TYPES': ('Bearer',)
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
