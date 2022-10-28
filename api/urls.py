1d1df731d07dcb86502c0c70eb2d0cfa3ac0ffda
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-27 22:07:32 -0300

    Criando as rotas do UserViewSet e incluindo.

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 app_name = "api"
+router = SimpleRouter()
+router.register("users", views.UserViewSet, "api_user")
 urlpatterns = [
+    path('', include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
