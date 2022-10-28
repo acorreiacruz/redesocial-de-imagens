702f0cc36dd755f9459b9a4624f8290a089746ec
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-27 22:29:35 -0300

    Alterando a view do User para ModelViewSet.

--- api/views.py
+++ api/views.py
@@ -5,23 +5,19 @@ from rest_framework import status
 from .serializers import UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, RetrieveModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import User
 
 
-class UserViewSet(CreateModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated,]
+    # permission_classes = [IsAuthenticated,]
     http_method_names = ["post", "patch", "get", "head", "options", "delete"]
 
     def get_object(self, *args, **kwargs):
-        print(self.kwargs)
-        return super().get_object(*args, **kwargs)
-
-    def get_queryset(self, *args, **kwargs):
-        return super().get_queryset(*args, **kwargs)
+        return super().get_object(self, *args, **kwargs)
 
 
 
