17242c033c5e635336d7b205dd396f32bbf7e1ad
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-28 10:52:37 -0300

    Adicioando permissão IsAuthenticated ao UserViewSet.

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from .models import User
 from .serializers import ProfileSerializer, UserSerializer
-from rest_framework.decorators import api_view
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.permissions import IsAuthenticated
@@ -10,11 +9,11 @@ from .models import User, Profile
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAuthenticated,]
     http_method_names = ["post", "patch", "get", "head", "options", "delete"]
 
     def get_object(self, *args, **kwargs):
-        return super().get_object(self, *args, **kwargs)
+        return super().get_object(*args, **kwargs)
 
 
 class ProfileViewSet(GenericViewSet, UpdateModelMixin, RetrieveModelMixin):
