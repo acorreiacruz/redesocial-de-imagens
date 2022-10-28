dd5be5615e1db8b6785d8056c2ab85923e808f28
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-28 10:41:37 -0300

    Definindo o CustomJWTSerializer como o serializer do TokenObtainPairView.

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from .serializers import CustomJWTSerializer
+
 
 
 app_name = "api"
@@ -10,7 +12,11 @@ router.register("users", views.UserViewSet, "api-user")
 router.register("profiles", views.ProfileViewSet, "api-profile")
 urlpatterns = [
     path('', include(router.urls)),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "token/",
+        TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer),
+        name="token_obtain_pair_email"
+    ),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
