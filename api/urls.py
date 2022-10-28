ff3919325298ab99bfa779b3ba70d939e352bad6
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-27 22:05:46 -0300

    Definindo as urls do Simple JWT.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
-from urllib.parse import urlparse
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-app_name = "users"
 
-urlpatterns = []
+app_name = "api"
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
