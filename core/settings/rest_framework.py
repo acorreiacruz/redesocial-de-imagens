509e371846adc97f0d5b2d8df86a237af5e403d1
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-28 10:22:46 -0300

    Cofigurando o authentication classes do rest_framework.

--- core/settings/rest_framework.py
+++ core/settings/rest_framework.py
@@ -7,6 +7,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
