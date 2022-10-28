e18959623de7020a97b56c665bc67a69bc2c080a
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-27 23:08:00 -0300

    Criando a ProfileViewSet.

--- api/views.py
+++ api/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import get_object_or_404
 from .models import User
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import UserSerializer
+from .serializers import ProfileSerializer, UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from .models import User
+from .models import User, Profile
 
 
 class UserViewSet(ModelViewSet):
@@ -18,6 +15,12 @@ class UserViewSet(ModelViewSet):
 
     def get_object(self, *args, **kwargs):
         return super().get_object(self, *args, **kwargs)
+
+
+class ProfileViewSet(GenericViewSet, UpdateModelMixin, RetrieveModelMixin):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    http_method_names = ['get', "patch", "options", "head"]
 
 
 
