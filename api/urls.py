b1e5e6d6b6a7c003d8b63aa75f295e8bd436bc45
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-04 20:20:46 -0300

    Registrando urls para a cbv RemarkViewSet.

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, include
-from . import views
+from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from . import views
 from .serializers import CustomJWTSerializer
-
-
 
 app_name = "api"
 
@@ -13,16 +15,33 @@ router.register("users", views.UserViewSet, "api-user")
 router.register("profiles", views.ProfileViewSet, "api-profile")
 router.register("posts", views.PostViewSet, "api-post")
 router.register("postlikes", views.PostLikeViewSet, "api-postlike")
+router.register("remarks", views.RemarkViewSet, "api-remark")
 
 urlpatterns = [
-    path("users/<int:id1>/follow/<int:id2>/", views.follow_user, name="follow_user"),
-    path("users/<int:id1>/unfollow/<int:id2>/", views.unfollow_user, name="unfollow_user"),
-    path('', include(router.urls)),
+    path(
+        "users/<int:id1>/follow/<int:id2>/",
+        views.follow_user,
+        name="follow_user"
+    ),
+    path(
+        "users/<int:id1>/unfollow/<int:id2>/",
+        views.unfollow_user,
+        name="unfollow_user"
+    ),
+    path("", include(router.urls)),
     path(
         "token/",
         TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer),
-        name="token_obtain_pair_email"
+        name="token_obtain_pair_email",
     ),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
+    path(
+        "token/verify/",
+        TokenVerifyView.as_view(),
+        name="token_verify"
+    ),
 ]
