250497728c8d7f92013cabcb6cab350292be33c7
acorreiacruz <antoniocruzjunior@outlook.com>
2023-06-12 11:31:22 -0300

    update: setting database configuration.

--- back/core/settings/databases.py
+++ back/core/settings/databases.py
@@ -1,9 +1,12 @@
-from .enviroments import BASE_DIR
-
+import os
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("DATABASE_ENGINE"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD")
     }
 }
